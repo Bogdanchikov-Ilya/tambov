65fcca6337b0475c884eb4617d88912023b9c8a5
Bogdanchikov-Ilya <test200373@gmail.com>
2021-12-26 14:24:20 +0300

    checkbox styles

--- src/pages/registration.vue
+++ src/pages/registration.vue
@@ -41,7 +41,7 @@
             <input type="tel" class="reg-item__input" id="phone" name="phone">
           </div>
           <div class="confirm">
-            <input type="checkbox" class="reg-item__input" id="confirm" name="confirm" checked>
+            <input type="checkbox" class="reg-item__input" id="confirm" name="confirm">
             <label class="popup-item__label" for="confirm">Я согласен на обработку моих данных</label>
           </div>
 
@@ -120,10 +120,43 @@ form{
       user-select: none;
     }
   }
+
+  //checkbox
+
   .confirm{
     margin-top: size(35, 1905);
     display: flex;
     align-items:center;
+    input{
+      position: absolute;
+      z-index: -1;
+      opacity: 0;
+    }
+    input+label {
+      display: inline-flex;
+      align-items: center;
+      user-select: none;
+    }
+    input+label::before {
+      content: '';
+      display: inline-block;
+      width: size(16, 1905);
+      height: size(16, 1905);
+      flex-shrink: 0;
+      flex-grow: 0;
+      border: 1px solid #C6CEDB;
+      border-radius: size(2, 1905);
+      margin-right: size(12, 1905);
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: 50% 50%;
+    }
+    input:checked+label::before {
+      border-color: #0b76ef;
+      background-color: transparent;
+      box-shadow: 0px 0px size(4, 1905) rgba(79, 140, 255, 0.25);
+      background-image: url("../assets/img/checkbox-confirm-icon.svg");
+    }
     label{
       font-size: size(12, 1905);
       line-height: size(16, 1905);
@@ -134,6 +167,7 @@ form{
 .register{
   margin-top: size(39, 1905);
 }
+
 
 @media (max-width: 744px){
   .reg-wrapper{
@@ -160,10 +194,18 @@ form{
     }
     .confirm{
       margin-top: size(35, 744);
+      input+label::before {
+        width: size(16, 744);
+        height: size(16, 744);
+        border-radius: size(2, 744);
+        margin-right: size(12, 744);
+      }
+      input:checked+label::before {
+        box-shadow: 0px 0px size(4, 744) rgba(79, 140, 255, 0.25);
+      }
       label{
         font-size: size(12, 744);
         line-height: size(16, 744);
-        color: #000000;
       }
     }
   }
@@ -196,6 +238,15 @@ form{
     }
     .confirm{
       margin-top: size(21, 320);
+      input+label::before {
+        width: size(16, 320);
+        height: size(16, 320);
+        border-radius: size(2, 320);
+        margin-right: size(12, 320);
+      }
+      input:checked+label::before {
+        box-shadow: 0px 0px size(4, 320) rgba(79, 140, 255, 0.25);
+      }
       label{
         font-size: size(10, 320);
         line-height: size(16, 320);
